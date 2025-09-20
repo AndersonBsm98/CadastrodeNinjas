b7b3100710df5a3697498b6fd73ca80a5243c686
Anderson Martins <andersonmartins03111998@gmail.com>
2025-09-19 21:12:06 -0300

    first commit

---
+++ src/main/java/dev/Panda10x/CadastroDeNinjas/Controller/NinjaController.java
@@ -0,0 +1,14 @@
+package dev.Panda10x.CadastroDeNinjas.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ninjas")
+public class NinjaController {
+
+    @GetMapping
+
+
+}
